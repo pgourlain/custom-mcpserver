22c7a749bb3685bf74283c6a4ab35b0f2cf4e517
Pierrick Gourlain <pgourlain@hotmail.com>
2025-05-18 08:08:44 +0200

    split of sse and stdio mcp

---
+++ stdio/Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Azure;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<StorageAccountTools>();
+builder.Services.AddSingleton<StorageService>();
+builder.Services.AddAzureClients(async clientBuilder =>
+{
+    var connectionString = builder.Configuration["ConnectionStrings:StorageAccount"];
+    
+    // Set a credential for all clients to use by default
+    // DefaultAzureCredential credential = new();
+    // clientBuilder.UseCredential(credential);
+    clientBuilder.AddTableServiceClient(connectionString);
+});
+await builder.Build().RunAsync();
+
---
+++ stdio/readme.md
@@ -0,0 +1,7 @@
+
+
+FAQ :
+help urls 
+- https://devblogs.microsoft.com/dotnet/build-a-model-context-protocol-mcp-server-in-csharp/#publish-your-mcp-server
+- https://github.com/jamesmontemagno/monkeymcp
+- 
